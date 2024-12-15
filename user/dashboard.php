aad670e6f29161ba2f6d27158e0b11a996772f2a
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-15 15:35:20 +0100

    first commit

---
+++ user/dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="min-h-screen flex justify-center items-center">
+    <div class="text-[30px] *:text-center">
+        <h1>User</h1>
+        <h2>Welcome, Ahmed Benkrara to your dashboard</h2>
+    </div>
+</body>
+</html>
